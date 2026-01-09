16a0cf84a7f3e29e17ac24a5b40af23f05e887c2
sada-02 <kartiksarda.2005@gmail.com>
2026-01-09 23:34:25 +0530

    paren

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,33 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
+
 string read_file_contents(const string& filename);
+
+struct tok{
+    string type;
+    string lexeme;
+    string literal;
+
+    tok(string t , string lex , string lit) : type {t} , lexeme {lex} , literal {lex} {}
+};
+
+vector<tok> Tokens;
+
+void handleTokenisation(string& file) {
+    for(char c : file) {
+        if(c == '(') {
+            Tokens.push_back({"LEFT_PAREN","(","NULL"});
+        }
+        else if(c == ')') {
+            Tokens.push_back({"RIGHT_PAREN",")","NULL"});
+        }
+    }
+
+    Tokens.push_back({"EOF","","NULL"});
+}
 
 int main(int argc, char *argv[]) {
     cout << unitbuf;
@@ -20,13 +45,16 @@ int main(int argc, char *argv[]) {
     const string command = argv[1];
 
     if (command == "tokenize") {
-        string file_contents = read_file_contents(argv[2]);
+        string file = read_file_contents(argv[2]);
         
-        if (!file_contents.empty()) {
+        if (!file.empty()) {
             cerr << "Scanner not implemented" << endl;
             return 1;
         }
-        cout << "EOF  null" << endl; 
+
+        for(auto& t : Tokens) {
+            cout<<t.type<<" "<<t.lexeme<<" "<<t.literal<<endl;
+        }
     } 
     else {
         cerr << "Unknown command: " << command << endl;
